798d9d1424542031ca8251a7b548e80fca6da6fe
dayamll <dayamllerenacastro@gmail.com>
2017-11-17 19:47:03 -0500

    Subiendo ultimos cambios,finalizando js

--- js/app.js
+++ js/app.js
@@ -31,3 +31,73 @@ function begin() {
       event.target.disabled = true;
     }
   }
+
+  function changeText(event) {
+    // si no existe, se asigna MAX
+    // si existe se habilita el boton y se resta el max con la longitud
+    if (event.target.value) {
+      var total = MAXCHARACTERS - event.target.value.length;
+
+      tweetBtnActive(true);
+
+      count.textContent = total;
+
+      changeColor(total);
+
+      checkEnters(event);
+      checkLong(event);
+      /* if (event.keyCode === 13)
+        event.target.rows = event.target.rows + 1; */
+    } else {
+      tweetBtnActive(false);
+      count.textContent = MAXCHARACTERS;
+    }
+  }
+
+  function changeColor(total) {
+    switch (true) {
+      case (total < 0):
+        tweetBtnActive(false);
+        count.classList.add('red');
+        count.classList.remove('orangered', 'greenyellow', 'seagreen');
+        break;
+      case (total <= 10):
+        count.classList.add('orangered');
+        count.classList.remove('red', 'greenyellow', 'seagreen');
+        break;
+      case (total <= 20):
+        count.classList.add('greenyellow');
+        count.classList.remove('red', 'orangered', 'seagreen');
+        break;
+      default:
+        count.classList.add('seagreen');
+        count.classList.remove('red', 'orangered', 'greenyellow');
+    }
+  }
+
+  // habilita el boton de tweet
+  function tweetBtnActive(centinel) {
+    tweetBtn.disabled = !centinel;
+  }
+
+  // verifica las filas del textarea, si sobrepasa
+  // se agrega una fila más, sino se elimina
+  function checkEnters(event) {
+    var text = event.target.value.split('');
+    var count = 0;
+
+    for (var i = 0; i < text.length; i++)
+      if (text[i] === '\n')
+        count++;
+
+    if (count)
+      event.target.rows = count + 2;
+  }
+
+  // agrega filas si el cociente entre los caracteres y las columnas del
+  // textarea, es menor a las filas del textarea actuales
+  function checkLong(event) {
+    if((event.target.value.length / event.target.cols) < event.target.rows)
+      event.target.rows = (event.target.value.length / event.target.cols) + 2;
+  }
+}
