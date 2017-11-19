6de51a01409195d13d12601da210cdc06d92f23d
dayamll <dayamllerenacastro@gmail.com>
2017-11-19 18:32:18 -0500

    Añadiendo crecimiento del text-area y cambios en css y html

--- js/app.js
+++ js/app.js
@@ -6,27 +6,26 @@ function begin() {
   var messages = document.getElementById('messages');
   var count = document.getElementById('count');
   var MAXCHARACTERS = 140;
-
   tweetBtn.addEventListener('click', message);
   tweetArea.addEventListener('keyup', changeText);
 
   function message(event) {
     event.preventDefault();
 
-    if (tweetArea.value) {
+    // if (tweetArea.value) {
+    if (tweetArea.value != '') {
       var div = document.createElement('div');
       var tweet = document.createElement('span');
-
       // agrega formato de hora
       tweet.innerHTML = tweetArea.value + '<i> Publicado: ' + moment().format('hh:mm') + '</i>';
       tweet.classList.add('tweet');
-
       div.classList.add('nuevo-mensaje');
       tweetArea.value = '';
       tweetArea.focus();
-
       div.appendChild(tweet);
       messages.insertBefore(div, messages.firstElementChild);
+      tweetArea.value = '';
+      tweetArea.focus();
     } else {
       event.target.disabled = true;
     }
@@ -35,17 +34,16 @@ function begin() {
   function changeText(event) {
     // si no existe, se asigna MAX
     // si existe se habilita el boton y se resta el max con la longitud
-    if (event.target.value) {
-      var total = MAXCHARACTERS - event.target.value.length;
-
+    if (event.target.value != '') {
       tweetBtnActive(true);
 
+      var writtenChars = event.target.value.length;
+      var total = MAXCHARACTERS - writtenChars;      
       count.textContent = total;
 
       changeColor(total);
-
       checkEnters(event);
-      checkLong(event);
+      // checkLong(event);
       /* if (event.keyCode === 13)
         event.target.rows = event.target.rows + 1; */
     } else {
@@ -54,18 +52,24 @@ function begin() {
     }
   }
 
-  function changeColor(total) {
+   function changeColor(total) {
+    // if(total < 0) {
+    //   tweetBtnActive(false);
+    //   count.classList.add('red');
+    //   count.classList.remove('orangered', 'greenyellow', 'seagreen');
+    //   return;
+    // }
     switch (true) {
-      case (total < 0):
+      case (total < 0): // cuando se supera el max
         tweetBtnActive(false);
         count.classList.add('red');
         count.classList.remove('orangered', 'greenyellow', 'seagreen');
         break;
-      case (total <= 10):
+      case (total <= 10): // a 10 chars del max
         count.classList.add('orangered');
         count.classList.remove('red', 'greenyellow', 'seagreen');
         break;
-      case (total <= 20):
+      case (total <= 20): // a 20 chars del max
         count.classList.add('greenyellow');
         count.classList.remove('red', 'orangered', 'seagreen');
         break;
@@ -79,25 +83,30 @@ function begin() {
   function tweetBtnActive(centinel) {
     tweetBtn.disabled = !centinel;
   }
-
   // verifica las filas del textarea, si sobrepasa
   // se agrega una fila más, sino se elimina
   function checkEnters(event) {
-    var text = event.target.value.split('');
-    var count = 0;
+    // var text = event.target.value.split('');
+    // var count = 0;
 
-    for (var i = 0; i < text.length; i++)
-      if (text[i] === '\n')
-        count++;
+    // for (var i = 0; i < text.length; i++)
+    //   if (text[i] === '\n')
+    //     count++;
+    
+    // if (count)
+    //   event.target.rows = count + 2;
+    if (event.keyCode === 13) {
+      event.target.style.height = "5px";
+      event.target.style.height = (event.target.scrollHeight) + "px";
+      // event.target.rows = event.target.rows + 2;
+      // console.log(event.target.rows)
+    }
 
-    if (count)
-      event.target.rows = count + 2;
   }
-
   // agrega filas si el cociente entre los caracteres y las columnas del
   // textarea, es menor a las filas del textarea actuales
   function checkLong(event) {
-    if((event.target.value.length / event.target.cols) < event.target.rows)
+    if ((event.target.value.length / event.target.cols) < event.target.rows)
       event.target.rows = (event.target.value.length / event.target.cols) + 2;
   }
 }
